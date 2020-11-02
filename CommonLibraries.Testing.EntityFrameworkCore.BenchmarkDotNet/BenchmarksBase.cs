beb9010d08944408d02a19a2d4a42354a8947700
Viktor Faddi <faddiv@gmail.com>
2020-11-02 15:07:15 +0100

    Migrate Testing.EntityFrameworkCore from CommonLibraries.

---
+++ CommonLibraries.Testing.EntityFrameworkCore.BenchmarkDotNet/BenchmarksBase.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
+using Perfolizer.Mathematics.OutlierDetection;
+
+namespace CommonLibraries.Testing.EntityFrameworkCore.BenchmarkDotNet
+{
+    [HtmlExporter, RPlotExporter]
+    [Orderer(SummaryOrderPolicy.Default)]
+    [RankColumn(NumeralSystem.Arabic)]
+    [Outliers(OutlierMode.RemoveAll)]
+    public class BenchmarksBase
+    {
+    }
+}
